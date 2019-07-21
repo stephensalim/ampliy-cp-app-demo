f4ef0c8b89b1f3f7119fdcb9c6fc186ef328f1f7
EC2 Default User <ec2-user@ip-172-31-20-81.us-west-2.compute.internal>
2019-07-21 18:47:13 +0000

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import awsconfig from './aws-exports';
+import Amplify, { API } from 'aws-amplify';
+import { withAuthenticator } from "aws-amplify-react";
+
+Amplify.configure(awsconfig);
+
+
+let apiName = 'cpappapi';
+let approvals = '/getapprovals';
+let sendapproval = '/signal';
+
+class App extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.handleApprove = this.handleApprove.bind(this);
+      this.handleDeny = this.handleDeny.bind(this);
+      this.state = {
+        resultHtml: "",
+        eventsSent: 0
+      };
+    }
+  
+    componentDidMount(){
+      API.get(apiName, approvals).then(response => {
+              var table = [];
+              var children = [];
+              console.log(response)
+              for(let i = 0; i < response.length; i++){
+                var execid = response[i].AutomationExecutionId;
+                children.push(<div id={execid} ><tr><td>{execid}</td><td><button value={execid} onClick={ this.handleApprove }> Approve </button></td><td><button value={execid} onClick={ this.handleDeny }> Deny </button></td></tr></div>)
+              }
+              table.push(<td>{children}</td>)
+              this.setState({
+                  resultHtml: table,
+              });
+      });
+    }
+  
+    handleApprove(event) {
+      let payload = {body:{id: event.target.value,decision: "Approve"}}
+      API.post(apiName, sendapproval, payload).then(response => {
+          console.log(response)
+      }).catch(error => {
+          console.log(error.response)
+      });
+      var el = document.getElementById(event.target.value);
+      el.remove();
+    }
+    
+    handleDeny(event) {
+      let payload = {body:{id: event.target.value,decision: "Reject"}}
+      API.post(apiName, sendapproval, payload).then(response => {
+          console.log(response)
+      }).catch(error => {
+          console.log(error.response)
+      });
+      var el = document.getElementById(event.target.value);
+      el.remove();
+    }
+    
+    render() {
+      return (
+        <div className="App">
+            <div>
+              <img src="https://aws-amplify.github.io/images/layout/logo.png" alt="logo"/>
+              <h1>Amplify Framework Demo</h1>
+              <center>
+              <table>
+                {this.state.resultHtml}
+              </table>
+              </center>
+            </div>
+        </div>
+      );
+  }
+  
+  
 }
 
-export default App;
+// export default App
+export default withAuthenticator(App , true);
